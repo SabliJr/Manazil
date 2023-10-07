02dd6387420e75eb7c8df106e6ef2018cbcfa154
Sabli Jr <sablijr@yahoo.com>
2023-10-07 23:47:51 +0200

    Making the Hero

---
+++ Frontend/src/Components/Hero/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Hero.css";
+
+const Index = () => {
+  return (
+    <main className='hero'>
+      <div className='heroDiv'>
+        <h3>Book your stay with Manazil</h3>
+        <p>1,480,086 rooms around the world are waiting for you!</p>
+      </div>
+    </main>
+  );
+};
+
+export default Index;
