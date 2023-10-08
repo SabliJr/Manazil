8ff17b9327fadaa4bc142b3de729ff42b16b36a2
Sabli Jr <sablijr@yahoo.com>
2023-10-08 18:23:44 +0200

    Finishing the choices section

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,12 +2,14 @@ import "./index.css";
 
 import Header from "./Components/Header/index";
 import Hero from "./Components/Hero/index";
+import Choices from "./Components/Choices/index";
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Hero />
+      <Choices />
     </div>
   );
 }
