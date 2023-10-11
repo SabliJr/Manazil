7df43eaea0267b0310f6e1752a2bf3a1989390d1
Sabli Jr <sablijr@yahoo.com>
2023-10-12 01:11:03 +0200

    Adding the Hotels data

--- Backend/src/PopularHotels/PopularHotels.ts
+++ Backend/src/PopularHotels/PopularHotels.ts
@@ -1,1 +1,10 @@
-import { booking } from "hotels-scraper-js";
+// import { airbnb, booking, hotelsCom } from "hotels-scraper-js";
+import axios from "axios";
+
+// booking.getHotels().then(console.log());
+const Hotels = () => {
+
+}
+
+
+module.exports = Hotels;
